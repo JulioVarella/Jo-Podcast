57f167ab95045853790e20df1d8ef58b58227c80
Julio Varella <julio.varella@unesp.br>
2021-03-25 01:39:17 -0300

    audio player e contador

--- js/main.js
+++ js/main.js
@@ -7,3 +7,40 @@ $('.main-slider').slick({
     autoplaySpeed: 2000
 });
 
+
+document.addEventListener("DOMContentLoaded", function () { startplayer(); }, false);
+var player;
+var count = 0;
+var aud = document.getElementById('music_player');
+
+/*function counter() {
+    if (aud.ended) {
+        count++;
+        document.getElementById("count").innerHTML = count;
+        aud.ended = false;
+    }
+}*/
+
+function startplayer() {
+    player = document.getElementById('music_player');
+    player.controls = false;
+}
+
+function play_aud() {
+    if(aud.ended){
+        count++;
+        document.getElementById("count").innerHTML = count;
+    }
+    player.play();
+    player.controls = false;
+}
+function pause_aud() {
+    player.pause();
+}
+function stop_aud() {
+    player.pause();
+    player.currentTime = 0;
+}
+function change_vol() {
+    player.volume = document.getElementById("change_vol").value;
+}
